8a095f73496426217a2eb3de64ea13253813efa9
proshunsuke <shunsuke0901@gmail.com>
2015-06-21 01:29:26 +0900

    テスト通す実装

--- src/fizzbuzz.rs
+++ src/fizzbuzz.rs
@@ -1,5 +1,7 @@
 pub fn fizzbuzz(n: i32) -> String {
-	if n % 3 == 0 {
+	if n % 3 == 0 && n % 5 == 0 {
+		"fizzbuzz".to_string()
+	} else if n % 3 == 0 {
 		"fizz".to_string()
 	} else if n % 5 == 0 {
 		"buzz".to_string()
